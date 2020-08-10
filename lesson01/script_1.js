fe82474dd2bdfd9286bfab85430e96c471a6976a
Yanis Kachinskis <yanis.kachinskis@gmail.com>
2020-08-10 20:05:34 +0300

    Урок 1. Задание выполнено.

---
+++ lesson01/script_1.js
@@ -0,0 +1,4 @@
+'use strict';
+let tempCels = prompt("Введите температуру в градусах Цельсия: ");
+let tempFar = (9 / 5) * tempCels + 32;
+alert("Температура в градусах Фаренгейта: " + tempFar);
---
+++ lesson01/script_2.js
@@ -0,0 +1,5 @@
+'use strict';
+let admin;
+let name = "Василий";
+admin = name;
+console.log(admin);
