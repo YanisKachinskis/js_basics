38fce8863fa960ae37d4a25112ca817c58397a5f
Yanis Kachinskis <yanis.kachinskis@gmail.com>
2020-08-25 19:46:42 +0300

    task_4 done

--- lesson05/script_4.js
+++ lesson05/script_4.js
@@ -2,18 +2,18 @@
 
 let buttons = document.querySelectorAll(".more");
 
-
-/*сделал смену картинки на текст, НЕ СЛОМАТЬ БЫ*/
-
 buttons.forEach(function(button){
-    let parent = button.parentNode; //.product
-    //console.log (parent.querySelector('.name'))
+    let parent = button.parentNode; 
     let img = parent.querySelector('.image');
     let desc = parent.querySelector('.description');
     button.addEventListener('click', function (){
-        img.classList.add('hidden');
-        desc.classList.remove('hidden');
+        img.classList.toggle('hidden');
+        desc.classList.toggle('hidden');
+        if (button.textContent === 'Подробнее') {
+            button.textContent = 'Отмена';
+        } else {
+            button.textContent = 'Подробнее';
+        };
     });
-    
 });
 
